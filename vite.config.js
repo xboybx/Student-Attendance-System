56d43ab0e65f273113dd158124ac5938a53a1233
Jaswanth <jeswanth012@gmail.com>
2024-12-25 15:15:23 +0530

    gh pages addition

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // base: '/Student-Attendance-System/',
+  base: '/Student-Attendance-System/',
   plugins: [react()],
   server: {
     port: 5173,
